e1c529b0d155fc2a931a5d3f721d278d24d27151
Manuel Galván <jesus.uanl.26@outlook.com>
2025-04-13 17:17:41 -0600

    Se agregó la funcionalidad del progress bar de la vida del enemigo, se descuenta vida cuando una pregunta es correctamente respondida

--- components/Enemy.tsx
+++ components/Enemy.tsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
 import { View, StyleSheet, Text} from "react-native";
 import { Image } from "expo-image";
+import ProgressBarLife from "./ProgressBarLife";
 
 const ImgEnemy = require('@/assets/images/characters/c7.png');
 
-export default function Enemy(){
-    const [life, setLife] = useState(0);
+export default function Enemy({lifes}:{lifes:number}){
+    const [life, setLife] = useState(1);
     const [enemyImage, setEnemyImage] = useState('');
     const [typeEnemy, setTypeEnemy] = useState('');
 
     return(
-        <Image
-            contentFit="contain"
-            style={styles.enemy}
-            source={ImgEnemy}
-        />
+        <View style={styles.container}>
+            <ProgressBarLife progress={lifes / 3}/>
+            <Image
+                contentFit="contain"
+                style={styles.enemy}
+                source={ImgEnemy}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10
+    },
     enemy: {
         width: 100,
-        height: 300,
-        alignSelf: 'flex-end',
-        margin: 10,
-        position: 'relative'
+        height: 150,
     }
 });
