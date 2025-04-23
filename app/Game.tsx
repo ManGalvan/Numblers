6977bd8fb45c3ddab55f710129a2444fe947faed
Manuel Galván <jesus.uanl.26@outlook.com>
2025-04-23 17:02:31 -0600

    La vida de los enemigos se incrementó a 10 de la misma manera que el total de preguntas a responder por juego, también se implementó un modal que le indicará al jugador cuando el juego haya terminado y le permita redirigirse al home (characters)

--- app/Game.tsx
+++ app/Game.tsx
@@ -1,19 +1,21 @@
-import { View, Text, StyleSheet, ImageBackground, Alert } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Alert, Modal, Pressable } from 'react-native';
 import { useState, useEffect } from 'react';
 import ProgressBar from '@/components/ProgressBar';
 import Player from '@/components/Player';
 import Enemy from '@/components/Enemy';
 import GameLogic from '@/components/GameLogic';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
 const bgImage = require('@/assets/images/bg.png');
 
 export default function Game() {
   const [operationType, setOperationType] = useState<string | null>(null);
   const [lifesPlayer, setLifesPlayer] = useState(3);
-  const [lifesEnemy, setLifesEnemy] = useState(3);
-  const [totalQuestions, setTotalQuestions] = useState(3);
+  const [lifesEnemy, setLifesEnemy] = useState(10);
+  const [totalQuestions, setTotalQuestions] = useState(10);
   const [questionsAnswered, setQuestionsAnswered] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const progress = questionsAnswered / totalQuestions;
 
@@ -35,8 +37,8 @@ export default function Game() {
   }, []);
 
   useEffect(() => {
-    if (progress === 1) {
-      Alert.alert('Juego terminado');
+    if (progress >= 1) {
+      setModalVisible(!modalVisible);
     }
   }, [progress]);
 
@@ -76,6 +78,22 @@ export default function Game() {
           </View>
         )
       }
+
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>El juego ha terminado! Has ganado 0 monedas</Text>
+            <Pressable style={[styles.button, styles.buttonCloseModal]} onPress={() => {router.navigate('/Characters')}}>
+              <Text style={styles.textButtonModal}>Salir</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 }
@@ -113,4 +131,40 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#fff',
   },
+  centeredView:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 15,
+    padding: 10,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  buttonCloseModal: {
+    backgroundColor: '#007AFF',
+    padding: 15
+  },
+  textButtonModal: {
+    textAlign: 'center',
+    color: '#fff'
+  }
 });
